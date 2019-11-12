338c88988d465624d437cbe23bcca3f2894ae575
jorgggge <55709952+jorgggge@users.noreply.github.com>
2019-11-12 12:56:41 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
-function Navegar(pagina) {
-	
-	if (pagina == 'Inicio') {
-		$('#Cuerpo').load('inicio.html');
-	}
-	if (pagina == 'Personajes') {
-		$('#Cuerpo').load('Personajes.html');
-	}
+
+
+function Inicio() 
+{
+	$('#L').hide();
+	$('#T').hide();
+	$('#P').hide();
+	$('#B').hide();
 }
+
+var Personaje = '';
+
+function Info(Personaje) 
+{
+	$('#M').hide();
+	$('#L').hide();
+	$('#T').hide();
+	$('#P').hide();
+	$('#B').hide();
+
+	if(Personaje == "M" )
+	{ 
+		$('#M').show(); 
+		document.getElementById('Cuerpo').style.backgroundImage = "url('M.png')";
+	}
+	if(Personaje == "L" )
+	{ 
+		$('#L').show(); 
+		document.getElementById('Cuerpo').style.backgroundImage = "url('L.png')";
+	}
+	if(Personaje == "T" )
+	{ 
+		$('#T').show(); 
+		document.getElementById('Cuerpo').style.backgroundImage = "url('T.png')";
+	}
+	if(Personaje == "P" )
+	{ 
+		$('#P').show();
+		document.getElementById('Cuerpo').style.backgroundImage = "url('P.png')";	
+	}
+	if(Personaje == "B" )
+	{ 
+		$('#B').show();
+		document.getElementById('Cuerpo').style.backgroundImage = "url('B.png')";
+	}
+
+}
+
